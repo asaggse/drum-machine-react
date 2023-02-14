bb96ac2661c0456697bf80f3a83b7c637d1d7eee
Alessandro Saggese <asaggese@gmail.com>
2023-02-14 13:06:15 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Pad from "./components/Pad"
+import Controls from "./components/Controls"
 
 function App() {
+  const [display, setDisplay] = useState('');
+
+  const updateDisplay = (soundName) => {
+    setDisplay(soundName);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="drum-machine">
+      <Pad updateDisplay={updateDisplay} />
+      <Controls display={display} />
     </div>
   );
 }
